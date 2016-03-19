b4947cd2624966c5929023d7c92080a5fd03d273
Péter Kardos <kardospeter1994@hotmail.com>
2016-03-19 19:00:58 +0100

    added stub for standard exceptions

---
+++ Engine/GraphicsApi_LL/Exception.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+
+// TODO: add common exception types for the graphics api
+
+
+namespace inl {
+namespace gxapi {
+
+
+// classes including but not limited to:
+//
+//	InvalidArgument
+//	InvalidState
+//	InvalidCall
+//	OutOfMemory
+//	FileNotFound
+//	ShaderCompilationError
+//	etc.
+
+
+
+} // namespace gxapi
+} // namespace inl
