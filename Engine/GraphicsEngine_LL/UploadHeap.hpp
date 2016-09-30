121ddd1aace2bcce34141f9d669481955c40365c
Artur Kovacs <kovacs.artur.barnabas@gmail.com>
2016-09-30 17:15:24 +0200

    Added offset for resource data upload

--- Engine/GraphicsEngine_LL/UploadHeap.hpp
+++ Engine/GraphicsEngine_LL/UploadHeap.hpp
@@ -11,17 +11,18 @@ namespace gxeng {
 class UploadHeap {
 public:
 	struct UploadDescription {
-		UploadDescription(GenericResource&& source, std::weak_ptr<GenericResource> destination) :
+		UploadDescription(GenericResource&& source, std::weak_ptr<GenericResource> destination, size_t offsetDst) :
 		source(std::move(source)), destination(destination) {}
 
 		GenericResource source;
 		std::weak_ptr<GenericResource> destination;
+		size_t offsetDst;
 	};
 
 public:
 	UploadHeap(gxapi::IGraphicsApi* graphicsApi);
 
-	void UploadToResource(std::weak_ptr<LinearBuffer> target, const void* data, size_t size);
+	void UploadToResource(std::weak_ptr<LinearBuffer> target, size_t offset, const void* data, size_t size);
 
 	std::vector<UploadDescription>& _GetQueuedUploads();
 
