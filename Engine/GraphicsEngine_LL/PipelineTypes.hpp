165c7b163a2574e61261cf25d2093465ae53a67a
kardo <kardospeter1994@hotmail.com>
2016-12-26 21:23:25 +0100

    nothing much

---
+++ Engine/GraphicsEngine_LL/PipelineTypes.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "ResourceView.hpp"
+
+namespace inl::gxeng::pipeline {
+
+
+class Texture {
+public:
+	//Texture(Texture2DSRV srv, RenderTargetView rtv, );
+
+};
+
+
+
+
+}
