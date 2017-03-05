ea80c4015395d23f191ac9c006d157464a35e963
Yours3lf <0.tamas.marton@gmail.com>
2017-03-05 22:42:57 +0000

    added inverse

--- Engine/GraphicsEngine_LL/Nodes/Shaders/DepthReductionFinal.hlsl
+++ Engine/GraphicsEngine_LL/Nodes/Shaders/DepthReductionFinal.hlsl
@@ -37,6 +37,37 @@ struct camera
 	float3 pos, view_dir, up_vector;
 };
 
+//TODO!!!!!!!!!!!!!!!!!!
+float4x4 inverse(float4x4 input)
+{
+#define minor(a,b,c) determinant(float3x3(input.a, input.b, input.c))
+	//determinant(float3x3(input._22_23_23, input._32_33_34, input._42_43_44))
+
+	float4x4 cofactors = float4x4(
+		minor(_22_23_24, _32_33_34, _42_43_44),
+		-minor(_21_23_24, _31_33_34, _41_43_44),
+		minor(_21_22_24, _31_32_34, _41_42_44),
+		-minor(_21_22_23, _31_32_33, _41_42_43),
+
+		-minor(_12_13_14, _32_33_34, _42_43_44),
+		minor(_11_13_14, _31_33_34, _41_43_44),
+		-minor(_11_12_14, _31_32_34, _41_42_44),
+		minor(_11_12_13, _31_32_33, _41_42_43),
+
+		minor(_12_13_14, _22_23_24, _42_43_44),
+		-minor(_11_13_14, _21_23_24, _41_43_44),
+		minor(_11_12_14, _21_22_24, _41_42_44),
+		-minor(_11_12_13, _21_22_23, _41_42_43),
+
+		-minor(_12_13_14, _22_23_24, _32_33_34),
+		minor(_11_13_14, _21_23_24, _31_33_34),
+		-minor(_11_12_14, _21_22_24, _31_32_34),
+		minor(_11_12_13, _21_22_23, _31_32_33)
+		);
+#undef minor
+	return transpose(cofactors) / determinant(input);
+}
+
 float linearize_depth(float depth, float near, float far)
 {
 	float A = -(far + near) / (far - near);
@@ -128,8 +159,7 @@ float4x4 efficient_shadow_split_matrix(int idx, float4x4 projection, float4x4 vi
 
 	float4x4 vp = projection * view;
 
-	//TODO!!!!!!!!!!!!!!!!!!
-	float4x4 inv_viewproj = vp;//inverse(vp);
+	float4x4 inv_viewproj = inverse(vp);
 	for (int c = 0; c < 8; ++c)
 	{
 		float4 trans = mul(inv_viewproj, float4(ndc_frustum_corners[c], 1));
