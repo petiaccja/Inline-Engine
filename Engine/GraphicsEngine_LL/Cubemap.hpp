dc63c874bbfb13ae65586d189db8496c13ab8113
Unknown <kardospeter1994@hotmail.com>
2017-11-15 17:22:48 +0100

    got so excited that it compiles i forgot to delete old code and add comments

--- Engine/GraphicsEngine_LL/Cubemap.hpp
+++ Engine/GraphicsEngine_LL/Cubemap.hpp
@@ -26,14 +26,30 @@ public:
 
 	Cubemap(MemoryManager* memoryManager, CbvSrvUavHeap* descriptorHeap) : ImageBase(memoryManager, descriptorHeap) {}
 
+	/// <summary> Allocates the underlying GPU-resident texture. </summary>
+	/// <param name="width"> Width of the texture in pixels. </param>
+	/// <param name="height"> Height of the texture in pixels. </param>
+	/// <param name="channelType"> The numeric representation of a pixel channel. See <see cref="ePixelChannelType/>. </param>
+	/// <param name="channelCount"> Number of channels per pixel. </param>
+	/// <param name="pixelClass"> How pixels are interpreted. See <see cref="ePixelClass"/>. </param>
 	void SetLayout(size_t width, size_t height, ePixelChannelType channelType, int channelCount, ePixelClass pixelClass);
+
+	/// <summary> Upload pixels as byte array to the GPU. </summary>
+	/// <param name="x"> Where to insert the block of uploaded pixels. Top-left corner. </param>
+	/// <param name="y"> Where to insert the block of uploaded pixels. Top-left corner. </param>
+	/// <param name="width"> Width of the uploaded pixel block. </param>
+	/// <param name="height"> Height of the uploaded pixel block. </param>
+	/// <param name="mipLevel"> Target mip level of the texture. </param>
+	/// <param name="face"> Which face of the cubemap to update. </param>
+	/// <param name="pixels"> A pointer to the bytes representing the pixels. Use <see cref="Pixel"/> as helper. </param>
+	/// <param name="reader"> Interprets byte stream. Implement <see cref="IPixelReader"/> or use <see cref="Pixel::Reader"/>. </param>
+	/// <param name="bytesPerRow"> How many bytes to skip in <paramref name="pixels"/> for each row. Leave as 0 for no row padding. </param>
 	void Update(size_t x, size_t y, size_t width, size_t height, int mipLevel, Face face, const void* pixels, const IPixelReader& reader, size_t bytesPerRow = 0);
 
 	const TextureViewCube& GetSrv();
 
-	void CreateResourceView(const Texture2D& texture) override;
-
 private:
+	void CreateResourceView(const Texture2D& texture) override;
 	static int GetFaceIndex(eAxis facePosition);
 
 private:
--- Engine/GraphicsEngine_LL/Image.hpp
+++ Engine/GraphicsEngine_LL/Image.hpp
@@ -1,10 +1,6 @@
 #pragma once
 
 #include <memory>
-//#include "MemoryObject.hpp"
-//#include "Pixel.hpp"
-//#include "MemoryManager.hpp"
-//#include "ResourceView.hpp"
 
 #include "ImageBase.hpp"
 
@@ -17,45 +13,34 @@ class Image : public ImageBase {
 public:
 	Image(MemoryManager* memoryManager, CbvSrvUavHeap* descriptorHeap) : ImageBase(memoryManager, descriptorHeap) {}
 
+	/// <summary> Allocates the underlying GPU-resident texture. </summary>
+	/// <param name="width"> Width of the texture in pixels. </param>
+	/// <param name="height"> Height of the texture in pixels. </param>
+	/// <param name="channelType"> The numeric representation of a pixel channel. See <see cref="ePixelChannelType/>. </param>
+	/// <param name="channelCount"> Number of channels per pixel. </param>
+	/// <param name="pixelClass"> How pixels are interpreted. See <see cref="ePixelClass"/>. </param>
 	void SetLayout(size_t width, size_t height, ePixelChannelType channelType, int channelCount, ePixelClass pixelClass);
-	void Update(size_t x, size_t y, size_t width, size_t height, int mipLevel, const void* pixels, const IPixelReader& reader, size_t bytesPerRow = 0);
 
+	/// <summary> Upload pixels as byte array to the GPU. </summary>
+	/// <param name="x"> Where to insert the block of uploaded pixels. Top-left corner. </param>
+	/// <param name="y"> Where to insert the block of uploaded pixels. Top-left corner. </param>
+	/// <param name="width"> Width of the uploaded pixel block. </param>
+	/// <param name="height"> Height of the uploaded pixel block. </param>
+	/// <param name="mipLevel"> Target mip level of the texture. </param>
+	/// <param name="pixels"> A pointer to the bytes representing the pixels. Use <see cref="Pixel"/> as helper. </param>
+	/// <param name="reader"> Interprets byte stream. Implement <see cref="IPixelReader"/> or use <see cref="Pixel::Reader"/>. </param>
+	/// <param name="bytesPerRow"> How many bytes to skip in <paramref name="pixels"/> for each row. Leave as 0 for no row padding. </param>
+	void Update(size_t x, size_t y, size_t width, size_t height, int mipLevel, const void* pixels, const IPixelReader& reader, size_t bytesPerRow = 0);
+	
 	const TextureView2D& GetSrv();
 
+private:
 	void CreateResourceView(const Texture2D& texture) override;
+
 private:
 	TextureView2D m_resourceView;
 };
 
-
-/* OLD CODE, REMOVE IF NEW WORKS
-class Image {
-public:
-	Image(MemoryManager* memoryManager, CbvSrvUavHeap* descriptorHeap);
-	~Image();
-
-	void SetLayout(size_t width, size_t height, ePixelChannelType channelType, int channelCount, ePixelClass pixelClass);
-	void Update(size_t x, size_t y, size_t width, size_t height, const void* pixels, const IPixelReader& reader, size_t bytesPerRow = 0);
-
-	size_t GetWidth();
-	size_t GetHeight();
-	ePixelChannelType GetChannelType() const;
-	int GetChannelCount() const;
-	ePixelClass GetPixelClass() const;
-
-	std::shared_ptr<const TextureView2D> GetSrv();
-protected:
-	static bool Image::ConvertFormat(ePixelChannelType channelType, int channelCount, ePixelClass pixelClass, gxapi::eFormat& fmt, int& resultingChannelCount);
-private:
-	std::shared_ptr<TextureView2D> m_resource;
-	ePixelChannelType m_channelType;
-	int m_channelCount;
-	ePixelClass m_pixelClass;
-	MemoryManager* m_memoryManager;
-	CbvSrvUavHeap* m_descriptorHeap;
-
-};
-*/
 
 
 } // namespace gxeng
