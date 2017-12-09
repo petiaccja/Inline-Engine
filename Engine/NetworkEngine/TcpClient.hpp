a26e13f0cb68d8e7d70856678612a5cb516cd1f5
The Doctor <xX-TheDoctor-Xx@users.noreply.github.com>
2017-12-09 16:12:01 +0000

    I forgot this

--- Engine/NetworkEngine/TcpClient.hpp
+++ Engine/NetworkEngine/TcpClient.hpp
@@ -18,14 +18,14 @@ public:
 	}
 
 	inline bool Connect(const IPAddress& addrStr) { return m_socket->Connect(addrStr); }
-	inline bool Close() { return m_socket->Close(); }
-	inline bool HasPendingData(uint32_t& pendingDataSize) { return m_socket->HasPendingData(pendingDataSize); }
-	inline bool Send(const uint8_t* data, int32_t count, int32_t& sent) { return m_socket->Send(data, count, sent); }
-	inline bool Recv(uint8_t* data, int32_t size, int32_t& read, SocketReceiveFlags flags = SocketReceiveFlags::None) { return m_socket->Recv(data, size, read, flags); }
-	inline bool Wait(SocketWaitConditions cond, std::chrono::milliseconds t) { return m_socket->Wait(cond, t); }
-	inline SocketConnectionState GetConnectionState() { return m_socket->GetConnectionState(); }
-	inline void GetAddress(IPAddress& outAddr) { return m_socket->GetAddress(outAddr); }
-	inline int32_t GetPortNo() { return m_socket->GetPortNo(); }
+	inline bool Close() const { return m_socket->Close(); }
+	inline bool HasPendingData(uint32_t& pendingDataSize) const { return m_socket->HasPendingData(pendingDataSize); }
+	inline bool Send(const uint8_t* data, int32_t count, int32_t& sent) const { return m_socket->Send(data, count, sent); }
+	inline bool Recv(uint8_t* data, int32_t size, int32_t& read, SocketReceiveFlags flags = SocketReceiveFlags::None) const { return m_socket->Recv(data, size, read, flags); }
+	inline bool Wait(SocketWaitConditions cond, std::chrono::milliseconds t) const { return m_socket->Wait(cond, t); }
+	inline SocketConnectionState GetConnectionState() const { return m_socket->GetConnectionState(); }
+	inline void GetAddress(IPAddress& outAddr) const { return m_socket->GetAddress(outAddr); }
+	inline int32_t GetPortNo() const { return m_socket->GetPortNo(); }
 
 private:
 	Socket *m_socket;
