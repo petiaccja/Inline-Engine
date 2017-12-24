60f632948394121fbc97104706bd019d291249ac
The Doctor <xX-TheDoctor-Xx@users.noreply.github.com>
2017-12-24 09:06:58 +0000

    Update

---
+++ Engine/NetworkEngine/Util.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Net.hpp"
+
+#include <vector>
+#include <sstream>
+
+namespace inl::net::util
+{
+	inline static std::vector<std::string> Split(const std::string &str, const std::string &delimiter)
+	{
+		std::vector<std::string> splited;
+		if (str.empty() && delimiter.empty())
+			return std::vector<std::string>();
+		std::string::size_type lastPos = str.find_first_not_of(delimiter, 0);
+		std::string::size_type pos = str.find_first_of(delimiter, lastPos);
+
+		while (std::string::npos != pos || std::string::npos != lastPos)
+		{
+			splited.push_back(str.substr(lastPos, pos - lastPos));
+			lastPos = str.find_first_not_of(delimiter, pos);
+			pos = str.find_first_of(delimiter, lastPos);
+		}
+		return splited;
+	}
+
+	sockaddr_in CreateAddress(uint32_t address, uint16_t port)
+	{
+		sockaddr_in addr;
+		std::memset(&addr, 0, sizeof(addr));
+		addr.sin_addr.s_addr = htonl(address);
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(port);
+
+		return addr;
+	}
+}
