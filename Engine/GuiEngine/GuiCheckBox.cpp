f63a807308199a199f1d80cf1a7654aba262338f
Dheeraj-Karki <dheerajkarki1790@outlook.com>
2018-01-29 00:48:53 +0530

    Adding initial code for check Box Item

---
+++ Engine/GuiEngine/GuiCheckBox.cpp
@@ -0,0 +1,34 @@
+﻿#include "GuiCheckBox.hpp"
+
+using namespace inl::gui;
+
+GuiCheckBox::GuiCheckBox(GuiEngine& guiEngine)
+    :Gui(guiEngine)
+{
+    checkBox = AddGui<GuiButton>();
+    checkBox->SetText(L"✓");
+    checkBox->AlignLeft();
+    checkBox->StretchFitToContent();
+    text = AddGui<GuiText>();
+    text->SetPosX(checkBox->GetPosX() + checkBox->GetWidth());
+    text->AlignVerCenter();
+}
+
+GuiCheckBox& GuiCheckBox::operator = (const GuiCheckBox& other)
+{
+    Gui::operator = (other);
+
+    text = Copy(other.text);
+
+    return *this;
+}
+
+void GuiCheckBox::SetText(const std::wstring& str)
+{
+    text->SetText(str);
+}
+
+void inl::gui::GuiCheckBox::SetText(const std::string & str)
+{
+    text->SetText(str);
+}
